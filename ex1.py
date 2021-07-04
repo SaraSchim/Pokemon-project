ca6a7c1f00c43a4981ed46effb2d824479ea1f1f
leahsilver <leah191711@gmail.com>
2021-07-04 12:32:27 +0300

    first part

---
+++ ex1.py
@@ -0,0 +1,16 @@
+from connection import connection
+
+def heaviest_pokemon():
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute( "select name from pokemon where weight = (SELECT MAX(weight) FROM pokemon);")
+            result = cursor.fetchall()
+            return result
+    except:
+        print("Error")
+
+
+
+
+
+
---
+++ ex3.py
@@ -0,0 +1,11 @@
+from connection import connection
+
+def find_owners(pokemon_name):
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute( "SELECT ownedBy.owner_name FROM pokemon JOIN ownedBy ON pokemon.id = ownedBy.pokemon_id where pokemon.name=(%s);",(pokemon_name))
+            result = cursor.fetchall()
+            return result
+    except:
+        print("Error")
+
