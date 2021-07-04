c20eef4803ba3dd174be52af3431d9a04c4806d6
leahsilver <leah191711@gmail.com>
2021-07-04 17:50:51 +0300

    first part

---
+++ json_to_sql.py
@@ -0,0 +1,18 @@
+from connection import connection
+import json
+def insert():
+    try:
+        with open("pokemon_data.json","r") as pokemon:
+            poekmon_list = json.load(pokemon)
+        for i in poekmon_list:
+                with connection.cursor() as cursor:
+                    cursor.execute( "INSERT INTO pokemon values(%s,%s,%s,%s);",(i["id"],i["name"],i["height"],i["weight"]))
+                    for j in i["ownedBy"]:
+                        
+                        if cursor.execute( "select name from owner where name=(%s);",(j["name"]))==0:
+                            cursor.execute( "INSERT INTO owner values(%s,%s);",(j["name"],j["town"]))
+                        cursor.execute( "INSERT INTO ownedBy values(%s,%s);",(i["id"],j["name"]))
+                    connection.commit()
+    except:
+        print("Error")
+insert()
